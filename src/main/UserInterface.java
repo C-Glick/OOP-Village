6ae5e5962e9987d4e61f29b763226727a16d28bf
TheChosenHero <thechosenherotch@gmail.com>
2018-07-13 22:12:01 -0500

    GUI mostly finished
    90% done writing gui, have to add commands yet and figure out why I
    can't change the print text color

--- src/main/UserInterface.java
+++ src/main/UserInterface.java
@@ -2,12 +2,18 @@ package main;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.UIManager;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
 
 
@@ -48,11 +54,33 @@ boolean trace = false;
 		console.setFont(new Font("Courier New", Font.PLAIN,12));
 		console.setOpaque(false);
 		
+		document = console.getStyledDocument();
+		
 		input = new JTextField();
 		input.setFont(new Font("Courier New", Font.PLAIN,12));
-		input.setCaretColor(Color.WHITE);
-		input.setForeground(Color.WHITE);
+		//input.setCaretColor(Color.WHITE);
+		//input.setForeground(Color.WHITE);
 		input.setOpaque(false);
+		
+		input.addActionListener(new ActionListener()
+		{
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					String text = input.getText();
+					if (text.length() > 1) {
+						doCommand(text);
+						scrollBottom();
+						input.selectAll();
+					}
+				}
+		});
+		
+		input.addKeyListener(new KeyListener()
+		{
+			public void keyPressed(KeyEvent e) {}
+			public void keyReleased(KeyEvent e) {}
+			public void keyTyped(KeyEvent e) {}
+		});
 		
 		scrollpane = new JScrollPane(console);
 		scrollpane.setBorder(null);
@@ -62,10 +90,62 @@ boolean trace = false;
 		frame.add(input, BorderLayout.SOUTH);
 		frame.add(scrollpane, BorderLayout.CENTER);
 		frame.setSize(660, 350);
-		frame.getContentPane().setBackground(new Color(50, 50, 50));
+	//	frame.getContentPane().setBackground(new Color(50, 50, 50));
 		frame.setLocationRelativeTo(null);
 		
 	
 		
 	}
+	public void doCommand(String s) {
+		final String[] commands = s.split(" ");
+		try {
+			if (commands [0].equalsIgnoreCase("clear")){
+				clear();
+			}
+			else {
+				print(s, false);
+	
+			}
+				
+			
+		}
+		catch(Exception ex){
+			print("Error -> "+ex.getMessage(), true);
+		}
+	}
+	public void scrollTop() {
+		console.setCaretPosition(0);
+	}
+	public void scrollBottom() {
+		console.setCaretPosition(console.getDocument().getLength());
+	}
+	public void print (String s, boolean trace) {
+		print(s,trace,new Color(255,255,255));
+	}
+	public void print (String s, boolean trace, Color c) {
+		Style style = console.addStyle("Style", null);
+		StyleConstants.setForeground(style, c);	
+		
+		if (trace) {
+			Throwable t = new Throwable();
+			StackTraceElement[] elements = t.getStackTrace();
+			String caller = elements[0].getClassName();
+			
+			s = caller + " -> " + s;
+		}
+		
+		try {
+			document.insertString(document.getLength(), s+"\n", style);
+			
+		}
+		catch (Exception ex) {
+		}
+			
+	}
+	public void clear() {
+		try {
+			document.remove(0, document.getLength());
+		}
+		catch(Exception ex){}
+		}
 }
